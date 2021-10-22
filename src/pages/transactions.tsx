03b13d35a8d2e2d557a219a32246329a187acd82
Vitor DevSP <contatodevsp@gmail.com>
2021-10-21 22:13:10 -0300

    feat: created comp. CardTransaction

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,11 +1,17 @@
-import { Heading, Stack } from "@chakra-ui/react"
+import { Flex, SimpleGrid, Stack, Text, Heading, Icon } from "@chakra-ui/react"
+import { FiArrowUp, FiArrowDown, FiDollarSign } from "react-icons/fi"
+import { CardTransaction } from "../components/CardTransaction"
 
 export default function Transactions() {
   return (
-    <Stack>
-      <Heading>1 - Salve</Heading>
-      <Heading>2 - Salve</Heading>
-      <Heading>3 - Salve</Heading>
+    <Stack paddingY={10}>
+      <SimpleGrid columns={3} spacing={10}>
+        <CardTransaction description="Entradas" title="R$ 1.400,00" icon="arrowUp" />
+        <CardTransaction description="Saídas" title="R$ 400,00" icon="arrowDown" />
+        <CardTransaction description="Total" title="R$ 1.000,00" icon="dollarSign" />
+      </SimpleGrid>
+
+      {/* TABLE */}
     </Stack>
   )
 }
