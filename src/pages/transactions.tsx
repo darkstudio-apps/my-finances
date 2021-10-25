909d29d3891fdbc666b43115abc634da614ea70c
Vitor DevSP <contatodevsp@gmail.com>
2021-10-25 18:17:55 -0300

    refactor: abstracting the data

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -3,18 +3,59 @@ import { getSession } from "next-auth/client"
 import { SimpleGrid, Stack } from "@chakra-ui/react"
 import { CardTransaction } from "../components/CardTransaction"
 import { TableTransaction } from "../components/TableTransaction"
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-} from "@chakra-ui/react"
+import { Table, Thead, Tbody, Tfoot, Tr, Th } from "@chakra-ui/react"
+
+interface TransactionsProps {
+  id: number
+  title: string
+  price: string
+  category: string
+  date: string
+  isIncome: boolean
+}
 
 export default function Transactions() {
+  const [transactions, setTransactions] = useState<TransactionsProps[]>([])
+
+  useEffect(() => {
+    const fetchTransactions = [
+      {
+        id: Date.now(),
+        title: "Desenvolvimento de site",
+        price: "R$ 12.000,00",
+        category: "Venda",
+        date: "13/04/2021",
+        isIncome: true,
+      },
+      {
+        id: Date.now(),
+        title: "Hamburguer",
+        price: "R$ 59,00",
+        category: "Alimentação",
+        date: "10/04/2021",
+        isIncome: false,
+      },
+      {
+        id: Date.now(),
+        title: "Computador",
+        price: "R$ 5.400,00",
+        category: "Venda",
+        date: "01/04/2021",
+        isIncome: true,
+      },
+      {
+        id: Date.now(),
+        title: "Aluguel do Apartamento",
+        price: "R$ 1.200,00",
+        category: "Casa",
+        date: "05/04/2021",
+        isIncome: false,
+      },
+    ]
+
+    setTransactions(fetchTransactions)
+  }, [])
+
   return (
     <Stack paddingY={10}>
       <SimpleGrid columns={3} spacing={10}>
@@ -34,6 +75,7 @@ export default function Transactions() {
           icon="dollarSign"
         />
       </SimpleGrid>
+
       <Stack paddingY={10}>
         <Table variant="simple" bg="gray.700" borderRadius="xl">
           <Thead>
@@ -44,36 +86,20 @@ export default function Transactions() {
               <Th>Data</Th>
             </Tr>
           </Thead>
+
           <Tbody>
-            <TableTransaction
-              title="Desenvolvimento de site"
-              price="R$ 12.000,00"
-              category="Venda"
-              date="13/04/2021"
-              isIncome={true}
-            />
-            <TableTransaction
-              title="Hamburguer"
-              price="R$ 59,00"
-              category="Alimentação"
-              date="10/04/2021"
-              isIncome={false}
-            />
-            <TableTransaction
-              title="Computador"
-              price="R$ 5.400,00"
-              category="Venda"
-              date="01/04/2021"
-              isIncome={true}
-            />
-            <TableTransaction
-              title="Aluguel do Apartamento"
-              price="R$ 1.200,00"
-              category="Casa"
-              date="05/04/2021"
-              isIncome={false}
-            />
+            {transactions.map((transaction) => (
+              <TableTransaction
+                key={transaction.id}
+                title={transaction.title}
+                price={transaction.price}
+                category={transaction.category}
+                date={transaction.date}
+                isIncome={transaction.isIncome}
+              />
+            ))}
           </Tbody>
+
           <Tfoot>
             <Tr>
               <Th></Th>
