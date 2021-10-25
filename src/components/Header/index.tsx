9f77b6d160aaf4961a68108d7341dc8e90305a4c
Vitor DevSP <contatodevsp@gmail.com>
2021-10-25 15:57:13 -0300

    feat: implementing authentication with google

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
-import { Avatar, Box, Flex, Heading, HStack, Text } from "@chakra-ui/react"
+import { Avatar, Box, Flex, Heading, HStack, Text, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react"
+import { signOut, useSession } from "next-auth/client"
 import { ActiveLink } from "./ActiveLink"
 
 export function Header() {
+  const [session] = useSession()
+
+  const handleSignOut = () => {
+    signOut()
+  }
+
   return (
     <Flex as="header" align="center" justify="space-between" paddingY={8}>
       <Heading size="lg" color="gray.200">My Finances</Heading>
@@ -12,12 +19,20 @@ export function Header() {
       </HStack>
 
       <HStack spacing={4}>
-        <Box >
-          <Heading fontSize="xl" fontWeight="semibold" color="gray.200">Vitor DevSP</Heading>
+        <Box>
+          <Heading fontSize="xl" fontWeight="semibold" color="gray.200">{session?.user?.name}</Heading>
           <Text fontSize="xs" color="gray.200">1 pendência</Text>
         </Box>
 
-        <Avatar name="Vitor DevSP" src="" />
+        <Menu>
+          <MenuButton>
+            <Avatar name={session?.user?.name || ""} src={session?.user?.image || ""} />
+          </MenuButton>
+
+          <MenuList>
+            <MenuItem onClick={handleSignOut}>Sair</MenuItem>
+          </MenuList>
+        </Menu>
       </HStack>
     </Flex>
   )
