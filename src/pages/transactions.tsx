a927373fab475e177ad58391fa5879bfa3fb445c
Vitor DevSP <contatodevsp@gmail.com>
2021-10-26 21:49:33 -0300

    fix: import deps in page transactions

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/client"
 import { SimpleGrid, Stack } from "@chakra-ui/react"
@@ -83,11 +84,7 @@ export default function Transactions() {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context)
 
-  console.log("session: ", session)
-
   if (!session) {
-    console.log("redirect: ", session)
-
     return {
       props: {},
       redirect: {
