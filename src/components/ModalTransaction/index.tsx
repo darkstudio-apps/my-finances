0fa30cfd7cbfbf489d47c7d8eb1150aca6e42b4e
jovemdan <danilogdev@gmail.com>
2021-10-28 22:17:06 -0300

    feat: implementation modal

--- src/components/ModalTransaction/index.tsx
+++ src/components/ModalTransaction/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef, useState, ChangeEventHandler } from "react"
 import {
   Modal,
   ModalOverlay,
@@ -12,16 +12,53 @@ import {
   HStack,
   Stack,
 } from "@chakra-ui/react"
-import { CheckBoxCard } from "../CheckBoxCard"
+import { CheckBoxCard, CheckBoxTypeProps } from "../CheckBoxCard"
 
 interface ModalTransactionProps {
   isOpen: boolean
   onClose: () => void
 }
 
+interface OnChangeProps {
+  target: {
+    name: string
+    value: string
+  }
+}
+
 export function ModalTransaction({ isOpen, onClose }: ModalTransactionProps) {
   const initialRef = useRef(null)
   const finalRef = useRef(null)
+
+  const [transaction, setTransaction] = useState({
+    title: "",
+    amount: 0,
+    dateTransaction: "",
+    category: "",
+  })
+
+  const [typeSelected, setTypeSelected] = useState<CheckBoxTypeProps | null>(null)
+
+  const handleChangeType = (type: CheckBoxTypeProps) => {
+    setTypeSelected(type)
+  }
+
+  const handleChangeTransaction = (event: OnChangeProps) => {
+    const { name, value } = event.target
+    setTransaction({
+      ...transaction,
+      [name]: value,
+    })
+  }
+
+  const handleSave = () => {
+    const transactionObj = {
+      ...transaction,
+      type: typeSelected,
+    }
+
+    console.log(transactionObj)
+  }
 
   return (
     <Modal
@@ -35,23 +72,62 @@ export function ModalTransaction({ isOpen, onClose }: ModalTransactionProps) {
       <ModalContent borderRadius="xl">
         <ModalHeader>Cadastrar Transação</ModalHeader>
         <ModalCloseButton />
+
         <ModalBody pb={6}>
           <Stack spacing={4}>
-            <Input ref={initialRef} placeholder="Nome" />
-
-            <Input placeholder="Preço" />
+            <Input
+              name="title"
+              value={transaction.title}
+              placeholder="Nome"
+              onChange={handleChangeTransaction}
+              ref={initialRef}
+            />
 
             <HStack spacing={2}>
-              <CheckBoxCard label="Entrada" icon="arrowUp"></CheckBoxCard>
-              <CheckBoxCard label="Saída" icon="arrowDown"></CheckBoxCard>
+              <Input
+                name="amount"
+                value={transaction.amount}
+                placeholder="Preço"
+                onChange={handleChangeTransaction}
+                type="number"
+              />
+
+              <Input
+                name="dateTransaction"
+                value={transaction.dateTransaction}
+                type="date"
+                placeholder="Preço"
+                onChange={handleChangeTransaction}
+              />
             </HStack>
 
-            <Input placeholder="Categoria" />
+            <HStack spacing={2}>
+              <CheckBoxCard
+                label="Entrada"
+                type="deposit"
+                checkedType={typeSelected}
+                onClick={handleChangeType}
+              />
+
+              <CheckBoxCard
+                label="Saída"
+                type="withdraw"
+                checkedType={typeSelected}
+                onClick={handleChangeType}
+              />
+            </HStack>
+
+            <Input
+              name="category"
+              value={transaction.category}
+              placeholder="Categoria"
+              onChange={handleChangeTransaction}
+            />
           </Stack>
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="blue" mr={3}>
+          <Button colorScheme="blue" mr={3} onClick={handleSave}>
             Save
           </Button>
           <Button onClick={onClose}>Cancel</Button>
