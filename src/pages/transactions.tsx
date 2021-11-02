549fdc3f6edca95d239e252df7eaa2d2d2bcfb22
Vitor DevSP <contatodevsp@gmail.com>
2021-11-02 12:35:32 -0300

    feat: created filters transactions

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/client"
+import { SimpleGrid, Stack, useDisclosure, Button, Select, HStack } from "@chakra-ui/react"
+
 import { CardTransaction } from "../components/CardTransaction"
 import { TableTransaction } from "../components/TableTransaction"
 import { SimpleGrid, Stack, useDisclosure, Button } from "@chakra-ui/react"
@@ -19,44 +21,16 @@ export default function Transactions() {
   const [transactions, setTransactions] = useState<TransactionsProps[]>([])
   const { isOpen, onOpen, onClose } = useDisclosure()
 
-  useEffect(() => {
-    const fetchTransactions = [
-      {
-        id: Date.now(),
-        title: "Desenvolvimento de site",
-        price: "R$ 12.000,00",
-        category: "Venda",
-        date: "13/04/2021",
-        isIncome: true,
-      },
-      {
-        id: Date.now(),
-        title: "Hamburguer",
-        price: "R$ 59,00",
-        category: "Alimentação",
-        date: "10/04/2021",
-        isIncome: false,
-      },
-      {
-        id: Date.now(),
-        title: "Computador",
-        price: "R$ 5.400,00",
-        category: "Venda",
-        date: "01/04/2021",
-        isIncome: true,
-      },
-      {
-        id: Date.now(),
-        title: "Aluguel do Apartamento",
-        price: "R$ 1.200,00",
-        category: "Casa",
-        date: "05/04/2021",
-        isIncome: false,
-      },
-    ]
+  const [filters, setFilters] = useState(() => {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = today.getMonth() + 1
 
-    setTransactions(fetchTransactions)
-  }, [])
+    return {
+      month,
+      year,
+    }
+  })
 
   return (
     <Stack paddingY={10} spacing={10}>
@@ -66,11 +40,13 @@ export default function Transactions() {
           title="R$ 1.400,00"
           icon="arrowUp"
         />
+
         <CardTransaction
           description="Saídas"
           title="R$ 400,00"
           icon="arrowDown"
         />
+
         <CardTransaction
           description="Total"
           title="R$ 1.000,00"
@@ -78,15 +54,53 @@ export default function Transactions() {
         />
       </SimpleGrid>
 
-      <Stack spacing={4}>
-        <Button
-          onClick={onOpen}
+      <Stack spacing={4} >
+        <HStack align="center" justify="space-between" spacing={4}>
+          <HStack spacing={4}>
+            <Select
+              name="month"
+              placeholder="Selecione o mês"
+              value={filters.month}
+              onChange={({ target: { name, value } }) => setFilters(old => ({ ...old, [name]: value }))}
+            >
+              <option value="1">Janeiro</option>
+              <option value="2">Fevereiro</option>
+              <option value="3">Março</option>
+              <option value="4">Abril</option>
+              <option value="5">Maio</option>
+              <option value="6">Junho</option>
+              <option value="7">Julho</option>
+              <option value="8">Agosto</option>
+              <option value="9">Setembro</option>
+              <option value="10">Outubro</option>
+              <option value="11">Novembro</option>
+              <option value="12">Dezembro</option>
+            </Select>
+
+            <Select
+              name="year"
+              placeholder="Selecione o ano"
+              value={filters.year}
+              onChange={({ target: { name, value } }) => setFilters(old => ({ ...old, [name]: value }))}
+            >
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+            </Select>
+          </HStack>
+
+          <Button
+            onClick={onOpen}
           maxWidth="200px"
-          borderRadius="md"
-          colorScheme="blue"
-        >
-          Nova Transação
-        </Button>
+            borderRadius="md"
+            colorScheme="blue"
+            paddingX={10}
+          >
+            Nova Transação
+          </Button>
+        </HStack>
 
         <TableTransaction data={transactions} />
       </Stack>
