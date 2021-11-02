93c33b139a7664d3a2cd64853ab7852a30f31c0b
Vitor DevSP <contatodevsp@gmail.com>
2021-11-02 12:30:35 -0300

    feat: created utils - generateID and maskUtil

---
+++ src/utils/maskUtil.ts
@@ -0,0 +1,8 @@
+export const formatCurrency = (amount: number) => {
+  return new Intl
+    .NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })
+    .format(amount)
+}
