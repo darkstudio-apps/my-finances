aec2fe94adcf3230feda5f6e5820b12b42d2f019
Vitor DevSP <contatodevsp@gmail.com>
2021-11-05 23:33:31 -0300

    feat: criando os recursos: list e get - em memoria

--- src/pages/api/transactions/_resources/repository/transactionRepository.ts
+++ src/pages/api/transactions/_resources/repository/transactionRepository.ts
@@ -1,3 +1,51 @@
-export const log = () => {
-  console.log("teste")
+const transactions = [
+  {
+    id: "63453252532",
+    title: "Desenvolvimento de site",
+    amount: 4000,
+    date: "2021-11-03T16:21:40.451Z",
+    type: "deposit",
+    category: "Venda",
+  },
+  {
+    id: "45353252532",
+    title: "Desenvolvimento de app",
+    amount: 3000,
+    date: "2021-11-05T16:21:40.451Z",
+    type: "deposit",
+    category: "Venda",
+  },
+]
+
+function list() {
+  return transactions
+}
+
+function get(idTransaction: string) {
+  return transactions.find(({ id }) => id === idTransaction)
+}
+
+function post() {
+
+}
+
+function put() {
+
+}
+
+function patch() {
+
+}
+
+function remove() {
+
+}
+
+export const transactionRepository = {
+  list,
+  get,
+  post,
+  put,
+  patch,
+  remove,
 }
--- src/pages/api/transactions/_resources/services/transactionService.ts
+++ src/pages/api/transactions/_resources/services/transactionService.ts
@@ -1,18 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { transactionRepository } from "../repository/transactionRepository"
 
 function list(req: NextApiRequest, res: NextApiResponse) {
-  const transactions = transactionRepository.list()
-  return res.status(200).json({ transactions })
+
 }
 
 function get(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query
-  let idTransaction = Array.isArray(id) ? id[0] : id
 
-  const transaction = transactionRepository.get(idTransaction)
-
-  return res.status(200).json({ transaction })
 }
 
 function post(req: NextApiRequest, res: NextApiResponse) {
@@ -31,7 +24,7 @@ function remove(req: NextApiRequest, res: NextApiResponse) {
 
 }
 
-export const transactionController = {
+export const transactionService = {
   list,
   get,
   post,
