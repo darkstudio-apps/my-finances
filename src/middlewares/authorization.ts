4384f4574b09892b8a7d6d166525c77a1c43f5f9
Vitor DevSP <contatodevsp@gmail.com>
2021-11-08 20:23:19 -0300

    refactor: moving folder middlewares

--- src/middlewares/authorization.ts
+++
@@ -1,8 +0,0 @@
-import jwt from "next-auth/jwt"
-
-const secret = process.env.JWT_SECRET
-
-import { getSession } from "next-auth/client"
-
-const token = await jwt.getToken({ req, secret })
-const session = await getSession({ req })
