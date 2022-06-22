0087516309740a442a4e61638fb1676e78feaf47
vitordevsp <contatodevsp@gmail.com>
2022-06-22 12:22:33 -0300

    refactor(TransactionAPI): deleta a estrutura de camadas atual

--- src/pages/api/_modules/transactions/services/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { generateDatesRecurrence } from "./handleDates"
-export { generateTransaction, generateTransactionToPost, generateTransactionsRecurrence } from "./handleTransactions"
-export { implementationRulesPut } from "./handleImplementationRulesPut"
